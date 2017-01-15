7ab4633c7969d2383aae936511c0a7dd39df7d98
Ingo <ingemar.strandberg@gmail.com>
2017-01-15 08:14:20 +0100

    Some refactoring...

---
+++ deploy-to-internal-nuget.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem nuget push .\src\RequestFiltering\bin\Release\RequestFiltering.1.0.0.nupkg -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal-releases/
+
+for /f "usebackq tokens=*" %%n in (`dir /b /s ".\src\RequestFiltering\bin\Release\*.nupkg"`) do nuget push "%%n" -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal/
+for /f "usebackq tokens=*" %%n in (`dir /b /s ".\src\RequestFiltering.Abstractions\bin\Release\*.nupkg"`) do nuget push "%%n" -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal/
+for /f "usebackq tokens=*" %%n in (`dir /b /s ".\src\RequestFiltering.FileExtensions\bin\Release\*.nupkg"`) do nuget push "%%n" -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal/
+for /f "usebackq tokens=*" %%n in (`dir /b /s ".\src\RequestFiltering.Headers\bin\Release\*.nupkg"`) do nuget push "%%n" -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal/
+for /f "usebackq tokens=*" %%n in (`dir /b /s ".\src\RequestFiltering.HiddenSegments\bin\Release\*.nupkg"`) do nuget push "%%n" -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal/
+for /f "usebackq tokens=*" %%n in (`dir /b /s ".\src\RequestFiltering.HttpVerbs\bin\Release\*.nupkg"`) do nuget push "%%n" -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal/
+for /f "usebackq tokens=*" %%n in (`dir /b /s ".\src\RequestFiltering.QueryStrings\bin\Release\*.nupkg"`) do nuget push "%%n" -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal/
+for /f "usebackq tokens=*" %%n in (`dir /b /s ".\src\RequestFiltering.Urls\bin\Release\*.nupkg"`) do nuget push "%%n" -Source https://nexus.strandberg.rocks:43571/repository/nuget-internal/
